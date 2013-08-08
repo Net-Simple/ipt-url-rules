2e365656c0adce4c99a609dbc8b2e9e55a33794f
Igor Timohin <itimohin@net-simple.ru>
2013-08-08 23:59:28 +0400

    pre-tested

--- iptables.sh
+++ iptables.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 IPT=/sbin/iptables
 echo "Stopping firewall and allowing everyone..."
-IPT -F
-IPT -X
-IPT -t nat -F
-IPT -t nat -X
-IPT -t mangle -F
-IPT -t mangle -X
-IPT -P INPUT ACCEPT
-IPT -P FORWARD ACCEPT
-IPT -P OUTPUT ACCEPT
+$IPT -F
+$IPT -X
+$IPT -t nat -F
+$IPT -t nat -X
+$IPT -t mangle -F
+$IPT -t mangle -X
+$IPT -P INPUT ACCEPT
+$IPT -P FORWARD ACCEPT
+$IPT -P OUTPUT ACCEPT
 
 # Max connection in seconds
 SECONDS=1
@@ -24,8 +24,12 @@ URL1="/v/users/login"
 DACTION="DROP"
 
 
-$IPT -A INPUT -m string --string ${URL} --algo kmp --to 65535 -m state --state NEW -m recent --set
-$IPT -A INPUT -m string --string ${URL} --algo kmp --to 65535 -m state --state NEW -m recent --update --seconds ${SECONDS} --hitcount ${BLOCKCOUNT} -j ${DACTION}
+$IPT -A INPUT -p tcp -m multiport --dports 80,443 -m string --string ${URL} --algo kmp -m state --state NEW -m recent --set
+$IPT -A INPUT -p tcp -m multiport --dports 80,443 -m string --string ${URL} --algo kmp -m state --state NEW -m recent --update --seconds ${SECONDS} --hitcount ${BLOCKCOUNT} -j ${DACTION}
 
-$IPT -A INPUT -m string --string ${URL1} --algo kmp --to 65535 -m state --state NEW -m recent --set
-$IPT -A INPUT -m string --string ${URL1} --algo kmp --to 65535 -m state --state NEW -m recent --update --seconds ${SECONDS} --hitcount ${BLOCKCOUNT} -j ${DACTION}
+# Тестовое правило для полного запрета подключений с заданной маской
+# $IPT -A INPUT -p tcp -m multiport --dports 80,443 -m string --string ${URL} --algo kmp -m state --state NEW -j DROP
+
+# Тестовые правила аналогичные включенынм, но действующие НЕ только на новые подключения
+#$IPT -A INPUT -p tcp -m multiport --dports 80,443 -m string --string ${URL} --algo kmp -m recent --set
+#$IPT -A INPUT -p tcp -m multiport --dports 80,443 -m string --string ${URL} --algo kmp -m recent --update --seconds ${SECONDS} --hitcount ${BLOCKCOUNT} -j ${DACTION}
